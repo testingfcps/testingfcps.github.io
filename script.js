e46d2561b417866a50a1d4b968f0dbce0b4a2002
testingfcps <jarksmc@gmail.com>
2025-04-04 13:11:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Redirect after 5 seconds
+// Open a new tab with the desired link after 5 seconds
 setTimeout(function() {
-    window.location.href = "https://www.example.com";  // Change this URL to the site you want to redirect to
+    window.open("chrome-extension://adkcpkpghahmbopkjchobieckeoaoeem///main.js", "_blank"); // This will open the link in a new tab
 }, 5000);
